01eff5d903db603c77c5a3221f65987442121b2e
Gana_Tech <ganamaddilti@gmail.com>
2022-04-25 12:37:05 +0530

    Initcommit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_Gana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
